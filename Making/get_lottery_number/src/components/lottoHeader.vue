a11a04943d6299672ec9ebdc0128f0e56aaf727a
Chany <lyhy0310@gmail.com>
2017-07-19 12:29:09 +0900

    #01

---
+++ Making/get_lottery_number/src/components/lottoHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1 class="lotto-heading">Get Your Lotto Number</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  data: function data() {
+    return {
+
+    }
+  }
+}
+</script>
+<style lang="sass">
+.lotto-heading
+    color: #fff
+    text-align: center
+    font-size: 2em
+</style>
---
+++ Making/get_lottery_number/src/components/lottoNumberSetting.vue
@@ -0,0 +1,80 @@
+<template>
+  <nav>
+    <ul class="lottonumber-setting-list">
+      <li class="lottonumber-setting-list-item">
+        <span class="btn-make-lottonumber" @click="makeLottoNumber">Make LottoNumber
+          <span class="btn-make-lottonumber-icon">
+          <i class="fa fa-eercast" aria-hidden="true"></i>
+        </span>
+      </span>
+      </li>
+      <li class="lottonumber-setting-list-item" v-if="displaySaveButton">
+        <span class="btn-save-lottonumber" @click="saveLottoNumber">Save it!
+        <span class="btn-save-lottonumber-icon">
+          <i class="fa fa-floppy-o" aria-hidden="true"></i>
+        </span>
+      </span>
+      </li>
+      <li><span></span></li>
+      <li><span></span></li>
+    </ul>
+  </nav>
+</template>
+<script>
+export default {
+  name: "",
+  data: function data() {
+    return {
+      displaySaveButton: false,
+    }
+  },
+  methods: {
+    makeLottoNumber() {
+      this.$emit("makeLottoNumber");
+      this.displaySaveButton = true;
+    },
+    saveLottoNumber(){
+      this.$emit("saveLottoNumber");
+      this.displaySaveButton = false;
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+.make-number-icon
+  margin-left: 0.5em
+
+.lottonumber-setting-list
+  margin-top: 0.8em
+  margin-bottom: 0.8em
+  display: flex
+  align-items: center
+  justify-content: center
+  line-height: 2em
+  // background-color: #fff
+
+.lottonumber-setting-list-item
+  margin-right: 0.8em
+
+.btn-make-lottonumber, .btn-save-lottonumber
+  display: inline-block
+  background-color: #EDC6A8
+  line-height: 2em
+  padding-left: 0.5em
+  border-radius: 0.3em
+
+.btn-make-lottonumber-icon,.btn-save-lottonumber-icon
+  text-align: center
+  display: inline-block
+  width: 2em
+  height: 2em
+  border-radius: 0.3em
+  line-height: 2em
+
+.btn-make-lottonumber-icon
+  background-color: #E2591E
+
+.btn-save-lottonumber-icon
+  background-color: #f41212
+</style>
